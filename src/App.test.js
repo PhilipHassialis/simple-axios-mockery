4e0c23a447c55d181c43fb4b58de89f737141f10
Philip <philip.alexander.hassialis@gmail.com>
2021-07-22 16:14:15 +0300

    Mock axios example

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, act } from "@testing-library/react";
+import App from "./App";
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("renders 2 facts instead of ten because I mock it", () => {
+
+  const mock = new MockAdapter(axios);
+  mock
+  .onGet("https://api.spaceflightnewsapi.net/v3/articles?_limit=10")
+  .reply(200, [{title:"AAA"} , {title:"BBB"}]);
+
+  it("should render the app with 2 random data", async () => {
+    await act(async () => {
+      render(<App />);
+    });
+    expect(screen.getByText('AAA')).toBeInTheDocument();
+  });
 });
